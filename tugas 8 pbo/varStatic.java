8cddbf53e60a47dc51d4f999e0e44d04960cd359
Nur Ainun Zulkarnain <62009449+13020180030@users.noreply.github.com>
2020-05-05 05:36:40 -0700

    Add files via upload

---
+++ tugas 8 pbo/varStatic.java
@@ -0,0 +1,9 @@
+public class varStatic {
+    public static void main(String[] args){
+        String firstName = "NUR AINUN ";
+        String lastName = "ZULKARNAIN";
+        String FULLname = firstName + lastName;
+        
+        System.out.println(FULLname);
+    }
+}
